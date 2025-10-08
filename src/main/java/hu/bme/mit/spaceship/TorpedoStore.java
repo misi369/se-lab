05964ed9fb49a35513fbcbf4fd6ab7837ba33f75
unknown <189764@bme.hu>
2025-10-08 14:56:54 +0200

    BugFix#4

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -17,7 +17,7 @@ public class TorpedoStore {
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
-    generator = = new Random();
+    generator = new Random();
 
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
